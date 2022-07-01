6345cc4e6b5283ddc2c8b883674b4c5e4bd3a2bf
aapozd <aapozdnyakove@gmail.com>
2022-07-01 16:50:44 +0300

    Translate incoming magic packet to outcoming
    Signed-off-by: aapozd <aapozdnyakove@gmail.com>

--- main/main.go
+++ main/main.go
@@ -6,40 +6,87 @@ import (
 	"encoding/hex"
 	"net"
 	"errors"
+	"bytes"
 )
 
 func main() {
 	if len(os.Args) != 2 {
-		fmt.Errorf("%v\n", "Invalid count of arguments")
+		fmt.Printf("%v\n", "Invalid count of arguments")
 		return
 	}
 
 	targetMAC := os.Args[1]
+
+	udpAddr, err := net.ResolveUDPAddr("udp4", "192.168.0.111:9")
+	conn, err := net.ListenUDP("udp4", udpAddr)
+
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		return
+	}
+
+	defer conn.Close()
+
+	myMac, _ := stringToMac("1C-BF-C0-C0-82-23")
+
+	for {
+		var n int
+
+		fmt.Println("Start main loop")
+
+		recvBuf := make([]byte, 258)
+
+		n, _, err = conn.ReadFromUDP(recvBuf)
+
+		if err != nil {
+			fmt.Printf("Cannot read from connection: %v\n", err)
+			return
+		}
+
+		fmt.Printf("Read %v bytes: %v\n", n, recvBuf)
+
+		if bytes.Equal(recvBuf[0 : 6], []byte{0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}) && (n == 102) {
+			fmt.Println("Found sync sequence")
+			isMagicPacket := true
+
+			for i := 6; i < 102; i += 6 {
+				isMagicPacket = isMagicPacket && bytes.Equal(recvBuf[i : i + 6], myMac)
+			}
+
+			if isMagicPacket {
+				sendMagicPacket(targetMAC)
+			}
+		}
+	}
+
+
+}
+
+func sendMagicPacket(targetMAC string) error {
 	byteMac, err := stringToMac(targetMAC)
 
 	if err != nil {
-		fmt.Errorf("%v\n", "TargetMAC is invalid: %v", err)
-		return
+		return err
 	}
 
 	packet := genMagicPacket(byteMac)
 
 	local,err := net.ResolveUDPAddr("udp4", ":9")
 	destinationAddress, err := net.ResolveUDPAddr("udp4", "192.168.0.255:9")
-	connection, err := net.DialUDP("udp4", local, destinationAddress)
-	defer connection.Close()
+	conn, err := net.DialUDP("udp4", local, destinationAddress)
+	defer conn.Close()
 
 	if err != nil {
-		fmt.Errorf("%v\n", "Unable to create socket:%v", err)
-		return
+		return err
 	}
 
-	_, err = connection.Write(packet)
+	_, err = conn.Write(packet)
 
 	if err != nil {
-		fmt.Errorf("%v\n", "Unable to write to the socker: %v", err)
-		return
+		return err
 	}
+
+	return nil
 }
 
 func stringToMac(stringMac string) ([]byte, error) {
