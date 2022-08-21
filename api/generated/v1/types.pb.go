4ff8be337f0ad0af5ae6b22957c9c77c3f966bd1
aaletov <aapozdnyakove@gmail.com>
2022-08-21 15:40:07 +0000

    Move Specs to operator
    Signed-off-by: aaletov <aapozdnyakove@gmail.com>

--- api/generated/v1/types.pb.go
+++ api/generated/v1/types.pb.go
@@ -68,104 +68,6 @@ func (x *WakeUpRequest) GetMAC() string {
 	return ""
 }
 
-// Should be reconciled inside operator
-//+kubebuilder:object:generate=true
-type WakeNodeUpRequest struct {
-	state         protoimpl.MessageState
-	sizeCache     protoimpl.SizeCache
-	unknownFields protoimpl.UnknownFields
-
-	MAC string `protobuf:"bytes,1,opt,name=MAC,proto3" json:"MAC,omitempty"`
-}
-
-func (x *WakeNodeUpRequest) Reset() {
-	*x = WakeNodeUpRequest{}
-	if protoimpl.UnsafeEnabled {
-		mi := &file_types_proto_msgTypes[1]
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		ms.StoreMessageInfo(mi)
-	}
-}
-
-func (x *WakeNodeUpRequest) String() string {
-	return protoimpl.X.MessageStringOf(x)
-}
-
-func (*WakeNodeUpRequest) ProtoMessage() {}
-
-func (x *WakeNodeUpRequest) ProtoReflect() protoreflect.Message {
-	mi := &file_types_proto_msgTypes[1]
-	if protoimpl.UnsafeEnabled && x != nil {
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		if ms.LoadMessageInfo() == nil {
-			ms.StoreMessageInfo(mi)
-		}
-		return ms
-	}
-	return mi.MessageOf(x)
-}
-
-// Deprecated: Use WakeNodeUpRequest.ProtoReflect.Descriptor instead.
-func (*WakeNodeUpRequest) Descriptor() ([]byte, []int) {
-	return file_types_proto_rawDescGZIP(), []int{1}
-}
-
-func (x *WakeNodeUpRequest) GetMAC() string {
-	if x != nil {
-		return x.MAC
-	}
-	return ""
-}
-
-// Created by WolController
-//+kubebuilder:object:generate=true
-type AttainableNode struct {
-	state         protoimpl.MessageState
-	sizeCache     protoimpl.SizeCache
-	unknownFields protoimpl.UnknownFields
-
-	MAC string `protobuf:"bytes,1,opt,name=MAC,proto3" json:"MAC,omitempty"`
-}
-
-func (x *AttainableNode) Reset() {
-	*x = AttainableNode{}
-	if protoimpl.UnsafeEnabled {
-		mi := &file_types_proto_msgTypes[2]
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		ms.StoreMessageInfo(mi)
-	}
-}
-
-func (x *AttainableNode) String() string {
-	return protoimpl.X.MessageStringOf(x)
-}
-
-func (*AttainableNode) ProtoMessage() {}
-
-func (x *AttainableNode) ProtoReflect() protoreflect.Message {
-	mi := &file_types_proto_msgTypes[2]
-	if protoimpl.UnsafeEnabled && x != nil {
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		if ms.LoadMessageInfo() == nil {
-			ms.StoreMessageInfo(mi)
-		}
-		return ms
-	}
-	return mi.MessageOf(x)
-}
-
-// Deprecated: Use AttainableNode.ProtoReflect.Descriptor instead.
-func (*AttainableNode) Descriptor() ([]byte, []int) {
-	return file_types_proto_rawDescGZIP(), []int{2}
-}
-
-func (x *AttainableNode) GetMAC() string {
-	if x != nil {
-		return x.MAC
-	}
-	return ""
-}
-
 var File_types_proto protoreflect.FileDescriptor
 
 var file_types_proto_rawDesc = []byte{
@@ -174,17 +76,12 @@ var file_types_proto_rawDesc = []byte{
 	0x75, 0x66, 0x2f, 0x65, 0x6d, 0x70, 0x74, 0x79, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x22, 0x21,
 	0x0a, 0x0d, 0x57, 0x61, 0x6b, 0x65, 0x55, 0x70, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x12,
 	0x10, 0x0a, 0x03, 0x4d, 0x41, 0x43, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x03, 0x4d, 0x41,
-	0x43, 0x22, 0x25, 0x0a, 0x11, 0x57, 0x61, 0x6b, 0x65, 0x4e, 0x6f, 0x64, 0x65, 0x55, 0x70, 0x52,
-	0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x12, 0x10, 0x0a, 0x03, 0x4d, 0x41, 0x43, 0x18, 0x01, 0x20,
-	0x01, 0x28, 0x09, 0x52, 0x03, 0x4d, 0x41, 0x43, 0x22, 0x22, 0x0a, 0x0e, 0x41, 0x74, 0x74, 0x61,
-	0x69, 0x6e, 0x61, 0x62, 0x6c, 0x65, 0x4e, 0x6f, 0x64, 0x65, 0x12, 0x10, 0x0a, 0x03, 0x4d, 0x41,
-	0x43, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x03, 0x4d, 0x41, 0x43, 0x32, 0x46, 0x0a, 0x0d,
-	0x57, 0x6f, 0x6c, 0x43, 0x6f, 0x6e, 0x74, 0x72, 0x6f, 0x6c, 0x6c, 0x65, 0x72, 0x12, 0x35, 0x0a,
-	0x06, 0x57, 0x61, 0x6b, 0x65, 0x55, 0x70, 0x12, 0x11, 0x2e, 0x76, 0x31, 0x2e, 0x57, 0x61, 0x6b,
-	0x65, 0x55, 0x70, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x1a, 0x16, 0x2e, 0x67, 0x6f, 0x6f,
-	0x67, 0x6c, 0x65, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62, 0x75, 0x66, 0x2e, 0x45, 0x6d, 0x70,
-	0x74, 0x79, 0x22, 0x00, 0x42, 0x06, 0x5a, 0x04, 0x2e, 0x2f, 0x76, 0x31, 0x62, 0x06, 0x70, 0x72,
-	0x6f, 0x74, 0x6f, 0x33,
+	0x43, 0x32, 0x46, 0x0a, 0x0d, 0x57, 0x6f, 0x6c, 0x43, 0x6f, 0x6e, 0x74, 0x72, 0x6f, 0x6c, 0x6c,
+	0x65, 0x72, 0x12, 0x35, 0x0a, 0x06, 0x57, 0x61, 0x6b, 0x65, 0x55, 0x70, 0x12, 0x11, 0x2e, 0x76,
+	0x31, 0x2e, 0x57, 0x61, 0x6b, 0x65, 0x55, 0x70, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x1a,
+	0x16, 0x2e, 0x67, 0x6f, 0x6f, 0x67, 0x6c, 0x65, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62, 0x75,
+	0x66, 0x2e, 0x45, 0x6d, 0x70, 0x74, 0x79, 0x22, 0x00, 0x42, 0x06, 0x5a, 0x04, 0x2e, 0x2f, 0x76,
+	0x31, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -199,16 +96,14 @@ func file_types_proto_rawDescGZIP() []byte {
 	return file_types_proto_rawDescData
 }
 
-var file_types_proto_msgTypes = make([]protoimpl.MessageInfo, 3)
+var file_types_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
 var file_types_proto_goTypes = []interface{}{
-	(*WakeUpRequest)(nil),     // 0: v1.WakeUpRequest
-	(*WakeNodeUpRequest)(nil), // 1: v1.WakeNodeUpRequest
-	(*AttainableNode)(nil),    // 2: v1.AttainableNode
-	(*empty.Empty)(nil),       // 3: google.protobuf.Empty
+	(*WakeUpRequest)(nil), // 0: v1.WakeUpRequest
+	(*empty.Empty)(nil),   // 1: google.protobuf.Empty
 }
 var file_types_proto_depIdxs = []int32{
 	0, // 0: v1.WolController.WakeUp:input_type -> v1.WakeUpRequest
-	3, // 1: v1.WolController.WakeUp:output_type -> google.protobuf.Empty
+	1, // 1: v1.WolController.WakeUp:output_type -> google.protobuf.Empty
 	1, // [1:2] is the sub-list for method output_type
 	0, // [0:1] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
@@ -234,30 +129,6 @@ func file_types_proto_init() {
 				return nil
 			}
 		}
-		file_types_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
-			switch v := v.(*WakeNodeUpRequest); i {
-			case 0:
-				return &v.state
-			case 1:
-				return &v.sizeCache
-			case 2:
-				return &v.unknownFields
-			default:
-				return nil
-			}
-		}
-		file_types_proto_msgTypes[2].Exporter = func(v interface{}, i int) interface{} {
-			switch v := v.(*AttainableNode); i {
-			case 0:
-				return &v.state
-			case 1:
-				return &v.sizeCache
-			case 2:
-				return &v.unknownFields
-			default:
-				return nil
-			}
-		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -265,7 +136,7 @@ func file_types_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_types_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   3,
+			NumMessages:   1,
 			NumExtensions: 0,
 			NumServices:   1,
 		},
